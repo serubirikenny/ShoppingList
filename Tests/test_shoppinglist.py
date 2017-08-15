521216e2c2d405cdae71795fd3fc20baca5415ed
serubirikenny <kserubiri@gmail.com>
2017-08-15 17:14:39 +0300

    Test Libraries and Unit tests

--- Tests/test_shoppinglist.py
+++ Tests/test_shoppinglist.py
@@ -10,35 +10,35 @@ class ShopTestCase(unittest.TestCase):
 
     def test_create_shopping_list_successfully(self):
         initial_room_count = len(self.user.shopping_lists)
-        cuttlery_list = self.user.create_shopping_list('Cuttlery', 'fork')
+        cuttlery_list = self.user.create_shopping_list('Shoes', 'Open')
         self.assertTrue(cuttlery_list)
         new_room_count = len(self.user.shopping_list)
         self.assertEqual(new_room_count - initial_room_count, 1)
 
     def test_create_shopping_list_when_list_already_exists(self):
-        self.user.shopping_list = {'Shoes': ['flats']}
-        self.assertEqual(self.user.create_shopping_list('Shoe', 'flats'),
+        self.user.shopping_list = {'Shirts': ['polo']}
+        self.assertEqual(self.user.create_shopping_list('Shirts', 'polo'),
                          'list already exists!', msg='list already exists!')
 
     def test_update_shopping_list(self):
-        self.user.shopping_list = {'shoes': ['flats']}
+        self.user.shopping_list = {'shirts': ['polo']}
         self.assertEqual(self.user.update_shopping_list(
-            'shoes', 'flat_shoes'), {'flat_shoes': ['flats']})
+            'shirts', 'polo_shirts'), {'polo_shirts': ['polo']})
 
     def test_update_shopping_list_item(self):
-        self.user.shopping_list = {'shoes': ['flats']}
-        self.assertEqual(self.user.update_shopping_list_item('flats', 'flat'), {'shoes': ['flat']})
+        self.user.shopping_list = {'shirts': ['polo']}
+        self.assertEqual(self.user.update_shopping_list_item('polos', 'polo'), {'shirts': ['polo']})
 
     def test_read_shopping_list_items(self):
-        self.user.shopping_list = {'shoes': ['flats', 'highs']}
-        self.assertEqual(self.user.print_shopping_list('shoes'), ['flats', 'highs'])
+        self.user.shopping_list = {'shirts': ['polo', 'african']}
+        self.assertEqual(self.user.print_shopping_list('shirts'), ['polo', 'African'])
 
     def test_delete_shopping_list(self):
-        self.user.shopping_list = {'shoes': ['flats', 'highs'], 'grocery': ['onions', 'tomatoes']}
+        self.user.shopping_list = {'shirts': ['polo', 'african'], 'milk': ['powdered', 'natural']}
         self.assertEqual(self.user.delete_shopping_list(
-            'shoes'), {'grocery': ['onions', 'tomatoes']})
+            'shirts'), {'milk': ['powdered', 'natural']})
 
     def test_delete_shopping_list_item(self):
-        self.user.shopping_list = {'shoes': ['flats', 'highs'], 'grocery': ['onions', 'tomatoes']}
-        self.assertEqual(self.user.delete_shopping_list('shoes', 'flats'), {
-                         'shoes': ['highs'], 'grocery': ['onions', 'tomatoes']})
+        self.user.shopping_list = {'shirts': ['polo', 'african'], 'milk': ['powdered', 'natural']}
+        self.assertEqual(self.user.delete_shopping_list('shirts', 'polo'), {
+                         'shirts': ['african'], 'milk': ['powdered', 'natural']})
